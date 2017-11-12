266ba17249cdf675c8816b9123c42f6848b196e1
Malcolm Groves <macgroves@gmail.com>
2017-11-11 19:49:17 -0500

    initialized repository and setup pg database

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+
+  devise_for :users
+  root 'static_pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
