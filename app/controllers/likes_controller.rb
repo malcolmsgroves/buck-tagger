ddc8adc18922100ca172de5f0bcca686f40fb636
Malcolm Groves <macgroves@gmail.com>
2017-12-05 19:14:11 -0500

    Debugged the like ajax, pulling teeth

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,11 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @like = current_user.likes.create!(like_params)
     @like.create_notification!(actor_id: current_user.id,
                                recipient_id: @like.likeable.user.id)
+    @likeable = @like.likeable
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js
@@ -14,6 +15,7 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.find_by(like_params)
     current_user.likes.destroy(@like)
+    @likeable = @like.likeable
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js
